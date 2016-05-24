ae7db3579bb4b78744496d4aaafa5ec906e0d37d
imostovenko <irina.mostovenko@gmail.com>
2016-05-24 03:11:20 +0300

    Datomic to server.clj

--- src/jira/server.clj
+++ src/jira/server.clj
@@ -9,11 +9,6 @@
   (:gen-class))
 
 
-
-(def url (str "datomic:free://localhost:4334/jira"))
-(def conn (d/connect url))
-(def db (d/db conn))
-
 (defn req->body [req]
   (-> req :body slurp))
 
@@ -38,3 +33,71 @@
   (let [port (or (get args "--port") "8080")]
     (println "Starting server at port" port)
     (httpkit/run-server #'app {:port (Long/parseLong port)})))
+
+
+
+;;;; Datomic DB connection
+
+(def url (str "datomic:free://localhost:4334/jira"))
+(def conn (d/connect url))
+(defn db [] (d/db conn))
+
+
+;;;; -------- USERS ----------
+
+(defn get-users-deep []
+  (map #(d/pull (db) '[*] %)
+    (d/q '[:find [?e ...]
+           :in $
+           :where [?e :user/login]]
+      (db))))
+
+
+(defn get-users-ids []
+  (d/q '[:find [?e ...]
+         :in $
+         :where [?e :user/login]]
+    (db)))
+
+
+(defn add-user [login pass]
+  (let [user [{:db/id #db/id[:db.part/user]
+               :user/login login
+               :user/pass pass
+               :user/isAdmin false}]]
+    (d/transact conn user))
+  (get-users-deep))
+
+
+(defn delete-user [u-id]
+  (let [user [[:db.fn/retractEntity u-id]]]
+    (d/transact conn user))
+  (get-users-deep))
+
+
+;;;;-------- PROJECTS --------
+
+(defn get-projects-deep []
+  (map #(d/pull (db) '[*] %)
+    (d/q '[:find [?e ...]
+           :in $
+           :where [?e :project/title]]
+      (db))))
+
+(defn create-prj [title descr]
+  (let [prj [{:db/id #db/id[:db.part/user]
+              :project/title title
+              :project/description descr}]]
+    ;:project/author}]]
+    (d/transact conn prj))
+  (get-projects-deep))
+
+
+(defn get-projects-ids []
+  (d/q '[:find [?e ...]
+         :in $
+         :where [?e :project/title]]
+    (db)))
+
+
+
